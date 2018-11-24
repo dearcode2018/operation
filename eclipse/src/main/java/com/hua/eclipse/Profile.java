5996b3b2a6bb2fa820082bf2a4d0d21cc975d8cf
qianye.zheng <dearzqy@163.com>
2018-11-24 22:59:08 +0800

    no message

--- eclipse/src/main/java/com/hua/eclipse/Profile.java
+++ eclipse/src/main/java/com/hua/eclipse/Profile.java
@@ -14,15 +14,49 @@ package com.hua.eclipse;
 public class Profile
 {
 	
-	
+	public static void main(String[] args)
+	{
+		
+	}
+
 	/**
 	 * 
 	 * @description 
 	 * @param args
 	 * @author qianye.zheng
 	 */
-	public static void main(String[] args)
+	public static void main2(String[] args)
 	{
-		System.out.println("Profile.main()");
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true)
+				{
+					System.out.println("thread1");
+				}
+			}
+		});
+		t1.start();
+		
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true)
+				{
+					System.out.println("thread2");
+				}
+			}
+		});
+		t2.start();
+		
+		System.out.println("MultipleThread.multiple()");
+		
+		try
+		{	// 主线程休眠，避免程序提前结束
+			Thread.sleep(1000 * 1000);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
